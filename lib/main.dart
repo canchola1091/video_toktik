e017f1f72c9a950622f7161c7fc22e4942bb5a77
Eduardo Canchola <canchola700@gmail.com>
2024-04-02 16:02:14 -0600

    arquitectura limpia

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/infraestructure/datasources/local_video_datasource_impl.dart';
+import 'package:toktik/infraestructure/repositories/video_posts_repository_impl.dart';
 import 'package:toktik/presentation/providers/discover_provider.dart';
 import 'package:toktik/presentation/screens/discover/discover_screen.dart';
 
@@ -11,11 +13,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final videoPostRepository = VideoPostsRepositoryImpl(
+      videoPostDataSource: LocalVideoDataSource()
+    );
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
           lazy: false,
-          create: (_) => DiscoverProvider()..loadNextPage()
+          create: (_) => DiscoverProvider(videoRepository: videoPostRepository)..loadNextPage()
         )
       ],
       child: MaterialApp(
